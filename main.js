e9f3da415aa5ccd45520ac9e7d500c2c4f673354
PadjokeJ <jonatan@padjokej.dev>
2026-02-04 15:37:07 +0100

    nice question grid and fucntional

--- main.js
+++ main.js
@@ -5,31 +5,35 @@ fetch("./questions.json")
   .then((json) => {
     for (i in json.Categories) {
       board_div = document.getElementById("board");
-      console.log(i);
       
       col = document.createElement("div");
       col.id = i;
       col.classList.add("category");
 
+      p = document.createElement("p");
+      p.id = i + "_p";
+      p.classList.add("category_header");
+      p.innerText = i;
+
+      col.append(p);
       board_div.append(col);
 
       current = json["Categories"][i];
       for (j in current) {
-        console.log(current[j]);
-
         question = document.createElement("button");
         question.id = i + "_" + j;
         question.classList.add("questbutton");
-        question.value = j;
+        question.innerText = j;
 
         text = document.createElement("p");
         text.style.display = "none";
         text.id = i + "_" + j + "_q";
-        text.innerText = current[j];
+        text.innerText = current[j]["question"];
 
         ans = document.createElement("p");
         ans.style.display = "none";
         ans.id = i + "_" + j + "_a";
+        ans.innerText = current[j]["answer"];
 
         question.append(text);
         question.append(ans);
@@ -37,12 +41,28 @@ fetch("./questions.json")
 
         question.addEventListener("click", (event) => {
           id = event.target.id;
+          document.getElementById(id).classList.add("complete");
           qval = document.getElementById(id + "_q").innerText;
           aval = document.getElementById(id + "_a").innerText;
 
-          document.getElementById(question).innerText = qval;
-          document.getElementById(answer).innerText = aval;
+          document.getElementById("question").innerText = qval;
+          document.getElementById("answer").innerText = aval;
+
+          document.getElementById("question").classList.toggle("show");
         });
       }
     }
   });
+
+document.getElementById("question").addEventListener("click", (event) => {
+  ques = document.getElementById("question");
+  if (ques.classList.contains("show")) {
+    ques.classList.toggle("show");
+    document.getElementById("answer").classList.toggle("show");
+  }
+});
+
+document.getElementById("answer").addEventListener("click", (event) => {
+  document.getElementById("answer").classList.toggle("show");
+});
+
